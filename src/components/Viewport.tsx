e36159ead526e8cb1f7f1791bcd9b8e9c51ccb64
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-12 00:29:07 +0200

    working display

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -1,22 +1,46 @@
 // App.tsx
-import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
+import { Model } from '../data/db';
+import { useEffect, useState } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Group, Object3DEventMap } from 'three';
 
-// Custom component to load and render the GLTF model
-const Model: React.FC = () => {
-  const { scene } = useGLTF('/example.glb');
-  return <primitive object={scene} />;
+
+interface ViewportProps {
+  model: Model;
 }
 
-const Viewport: React.FC = () => {
+
+const SceneObject = ({ model }: ViewportProps) => {
+
+  const [scene, setScene] = useState<Group<Object3DEventMap> | null>(null);
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.parse(model.content, '', (gltf) => {
+      setScene(gltf.scene);
+    });
+  }, [model]);
+
+  return scene ? <primitive object={scene} /> : <></>;
+}
+
+const Viewport = ({ model }: ViewportProps) => {
+
+
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
-      <Model />
-      <OrbitControls />
-    </Canvas>
+    <div className='w-96 h-96'>
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <SceneObject model={model} />
+        <OrbitControls />
+        <gridHelper args={[10, 10]} />
+        <axesHelper args={[5]} />
+        <color attach="background" args={['#827b70']} />
+      </Canvas>
+    </div>
   );
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,11 @@
+
 export const isNumeric = (s: string) => /^[+-]?\d+(\.\d+)?$/.test(s)
+
+function isJsonString(str: string) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
