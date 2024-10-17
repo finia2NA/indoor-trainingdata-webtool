c7e9d78a34a74cd42f1e068f2437d1f754bb75c5
Finite <33845166+finia2NA@users.noreply.github.com>
2024-10-17 21:21:17 +0200

    switch almost working

--- src/components/Viewport/SwitchableCamera.tsx
+++ src/components/Viewport/SwitchableCamera.tsx
@@ -1,20 +1,39 @@
-import { OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import * as THREE from "three";
+import * as DREI from "@react-three/drei";
 import useEditorStore, { EditorState, Perspective, } from "../../hooks/useEditorState";
+import { useEffect, useRef } from "react";
+
 
 const SwitchableCamera = () => {
-
-  // console.log("RENDERING SWITCHABLE CAMERA");
-
+  console.log("RENDERING SWITCHABLE CAMERA");
   const { perspectiveMode } = useEditorStore((state) => (state as EditorState));
 
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+
+    const aspect = window.innerWidth / window.innerHeight;
+    const orthoMatrix = new THREE.OrthographicCamera(
+      -aspect, aspect, 1, -1, 0.1, 1000
+    ).projectionMatrix;
+    const perspMatrix = new THREE.PerspectiveCamera(
+      75, aspect, 0.1, 1000
+    ).projectionMatrix;
+
+    if (!cameraRef.current) return;
+    console.log(cameraRef.current);
+
+    cameraRef.current.projectionMatrix.copy(perspectiveMode === Perspective.PERSPECTIVE ? perspMatrix : orthoMatrix);
+
+  }, [cameraRef, perspectiveMode]);
 
   return (
-    <>
-      {perspectiveMode === Perspective.PERSPECTIVE ?
-        <PerspectiveCamera makeDefault position={[0, 0, 5]} /> :
-        <OrthographicCamera makeDefault position={[0, 0, 5]} />}
+    <DREI.PerspectiveCamera
+      makeDefault
+      position={[0, 0, 5]}
+      ref={cameraRef}
+    />
 
-    </>
   )
 }
 
