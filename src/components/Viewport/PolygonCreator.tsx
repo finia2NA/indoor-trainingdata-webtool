4ee3efd099d1f3be28ad9bb17a3b8773d9f837be
Finite <33845166+finia2NA@users.noreply.github.com>
2025-01-16 20:45:59 +0100

    no making points when dragging

--- src/components/Viewport/PolygonCreator.tsx
+++ src/components/Viewport/PolygonCreator.tsx
@@ -5,20 +5,34 @@ import { DoubleSide, Vector3 } from 'three';
 const PolygonCreator: React.FC = () => {
   const { height } = usePolygonCreatorStore((state) => state);
   const [points, setPoints] = useState<Vector3[]>([]);
+  const [isDragging, setIsDragging] = useState(false);
 
   const handlePointerDown = (event: any) => {
     event.stopPropagation();
-    const { x, y, z } = event.point;
-    setPoints([...points, new Vector3(x, y, z)]);
+    setIsDragging(false);
+  };
+
+  const handlePointerMove = () => {
+    setIsDragging(true);
+  };
+
+  const handlePointerUp = (event: any) => {
+    event.stopPropagation();
+    if (!isDragging) {
+      const { x, y, z } = event.point;
+      setPoints([...points, new Vector3(x, y, z)]);
+    }
   };
 
   return (
     <>
-    {/* surface */}
+      {/* surface */}
       <mesh
         rotation={[Math.PI / 2, 0, 0]}
         position={[0, height, 0]}
         onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
       >
         <planeGeometry args={[5, 5]} />
         <meshStandardMaterial
