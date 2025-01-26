a4895f9dd2007ae94cdbaa5a61d4168d61fa4beb
Finite <33845166+finia2NA@users.noreply.github.com>
2025-01-26 16:52:15 +0100

    enable react compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,8 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactCompiler from 'eslint-plugin-react-compiler'
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// const ReactCompilerConfig = { /* ... */ };
+const ReactCompilerConfig = {
+  target: '18' // '17' | '18' | '19'
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(
-    // {
-    //   babel: {
-    //     plugins: [
-    //       ["babel-plugin-react-compiler", ReactCompilerConfig]
-    //     ]
-    //   }
-    // }
-  )],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+    ],
+    // ...
+  };
+});
