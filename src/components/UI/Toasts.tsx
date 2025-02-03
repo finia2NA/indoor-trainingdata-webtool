07a2024c982b4cfe1c214a509df6db3bbe36e35c
Finite <33845166+finia2NA@users.noreply.github.com>
2025-02-03 23:21:38 +0100

    Add labels in graphs, add reset button

--- src/components/UI/Toasts.tsx
+++ src/components/UI/Toasts.tsx
@@ -1,6 +1,6 @@
 import { ToastContentProps } from 'react-toastify';
 
-const PolygonDeletionToast = ({ closeToast }: ToastContentProps) => {
+export const PolygonDeletionToast = ({ closeToast }: ToastContentProps) => {
   return (
     <div className='flex flex-col gap-2 align-right'>
       <p>Cannot delete a point in a triangle</p>
@@ -9,4 +9,11 @@ const PolygonDeletionToast = ({ closeToast }: ToastContentProps) => {
   )
 }
 
-export default PolygonDeletionToast;
+export const ResetConfirmationToast = ({ closeToast }: ToastContentProps) => {
+  return (
+    <div className='flex flex-col gap-2 align-right'>
+      <p>Are you sure you want to reset the generation settings?</p>
+      <button className='bg-red-500 rounded-md' onClick={() => closeToast("reset")}>Reset</button>
+    </div>
+  )
+}
