cec43612c3a245f58c12b4d78a0dcb56721c6ea1
Finite <33845166+finia2NA@users.noreply.github.com>
2025-02-09 21:33:35 +0100

    Switch to 2d triangulation

--- src/components/Viewport/PolygonCreator/TriangulizationDisplay.tsx
+++ src/components/Viewport/PolygonCreator/TriangulizationDisplay.tsx
@@ -1,42 +1,59 @@
-import { Vector3 } from "three";
-import earcut from 'earcut';
+import { Vector3, BufferAttribute, DoubleSide } from "three";
+import Triangulation from "../../../util/triangulate";
+import useEditorStore, { EditorMode } from "../../../hooks/useEditorStore";
 
-type TriangulizationDisplayProps = {
+type TriangulationDisplayProps = {
   polygon: Vector3[];
 }
 
-const TriangulizationDisplay = ({ polygon }: TriangulizationDisplayProps) => {
-  const lineIndices: number[][] = [];
-  // const a = polygon.flatMap((point) => [point.x, point.z]);
-  const a = polygon.flatMap((point) => [point.x, point.y, point.z]);
-  const earcutIndices = earcut(a, undefined, 3);
-
-  for (let i = 0; i < earcutIndices.length; i += 3) {
-    lineIndices.push([earcutIndices[i], earcutIndices[i + 1]]);
-    lineIndices.push([earcutIndices[i + 1], earcutIndices[i + 2]]);
-    lineIndices.push([earcutIndices[i + 2], earcutIndices[i]]);
-  }
-
-  const linePoints = lineIndices.map((line) => line.map((index) => polygon[index]));
+const TriangulationDisplay = ({ polygon }: TriangulationDisplayProps) => {
+  const triangulation = new Triangulation(polygon);
+  const { editorMode } = useEditorStore();
 
   return (
     <>
-      {linePoints.map((line, i) => (
-        <line key={i} >
+      {triangulation.lines.map((line, i) => (
+        <line key={`line-${i}`}>
           <bufferGeometry
             attach="geometry"
-            ref={(geometry) => geometry && geometry.setFromPoints(line)}
+            ref={(geometry) => geometry && geometry.setFromPoints(line.map((point) => point.position))}
           />
           <lineBasicMaterial
-          attach="material"
-          color="white"
-          // transparent opacity={0.2}
+            attach="material"
+            color="white"
           />
         </line>
-
       ))}
+
+      {editorMode === EditorMode.MAP && 
+          triangulation.triangles.map((triangle, i) => (
+            <mesh key={`triangle-${i}`}>
+              <bufferGeometry
+                attach="geometry"
+                ref={(geometry) => {
+                  if (!geometry) return;
+                  const positions = new Float32Array([
+                    triangle[0].position.x, triangle[0].position.y, triangle[0].position.z,
+                    triangle[1].position.x, triangle[1].position.y, triangle[1].position.z,
+                    triangle[2].position.x, triangle[2].position.y, triangle[2].position.z,
+                  ]);
+                  geometry.setAttribute('position', new BufferAttribute(positions, 3));
+                  geometry.setIndex([0, 1, 2]);
+                  geometry.computeVertexNormals();
+                }}
+              />
+              <meshBasicMaterial
+                attach="material"
+                color="blue"
+                side={DoubleSide}
+                transparent
+                opacity={0.5}
+              />
+            </mesh>
+          ))
+      }
     </>
   );
 }
 
-export default TriangulizationDisplay;
+export default TriangulationDisplay;
