a58d95a59de48863121f6e1c5c08f1318ee72bd8
Finite <33845166+finia2NA@users.noreply.github.com>
2025-08-15 04:47:06 +0200

    some comments

--- src/components/Viewport/View360Sphere.tsx
+++ src/components/Viewport/View360Sphere.tsx
@@ -7,28 +7,38 @@ type View360SphereProps = {
   selectedImage: Image360 | null;
 };
 
+/**
+ * This react-three-fiber component renders a 
+ */
+// React components are functions that return JSX, which describes the UI.
+// They take in outside data as props and can contain their own state.
 const View360Sphere = ({ selectedImage }: View360SphereProps) => {
+  // Global state can be accessed using helper functions, called hooks.
   const { is360ViewActive, sphereOpacity } = useCameraPoseStore();
+
+  // Component level state can be initiated using useState.
   const [texture, setTexture] = useState<THREE.Texture | null>(null);
 
+  // By using useEffect, we can execute code when the component is first displayed, or when the dependency array changes.
   useEffect(() => {
     if (selectedImage?.texture) {
       setTexture(selectedImage.texture);
     }
   }, [selectedImage]);
 
-  if (!is360ViewActive || !selectedImage || !texture) return null;
 
+  // The presentation of a react component is defined in its return statement.
+  if (!is360ViewActive || !selectedImage || !texture) return null;
   return (
-    <mesh 
+    <mesh
       position={[selectedImage.x, selectedImage.y, selectedImage.z]}
       rotation={[0, THREE.MathUtils.degToRad(selectedImage.course), 0]}
     >
       <sphereGeometry args={[0.5, 32, 32]} />
-      <meshBasicMaterial 
-        map={texture} 
-        transparent 
-        opacity={sphereOpacity} 
+      <meshBasicMaterial
+        map={texture}
+        transparent
+        opacity={sphereOpacity}
         side={THREE.DoubleSide}
       />
     </mesh>
